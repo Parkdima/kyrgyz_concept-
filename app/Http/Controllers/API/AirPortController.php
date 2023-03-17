3e5abdc348a943ce4273f21944315114d17beaa2
Parkdima <124587966+Parkdima@users.noreply.github.com>
2023-03-17 20:39:56 +0500

    endpoint

--- app/Http/Controllers/API/AirPortController.php
+++ app/Http/Controllers/API/AirPortController.php
@@ -1,18 +1,34 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\AirLine;
 use App\Models\AirPort;
 use App\Models\plane;
 use Illuminate\Http\Request;
+use function PHPUnit\Framework\isEmpty;
 
 class AirPortController extends Controller
 {
     public function index(Request $request)
     {
-        $splitReq = explode(' ', $request->code);
+//        if (isEmpty($request->all())){
+//            return response()->json(['msg' => 'Bad request'], 400);
+//        }
+        $splitReq = implode(' ', $request->all('code'));
+
+        $first_word = strtok($splitReq," ");
+        $count_letters = strlen($first_word);
+        if ($count_letters > 2)
+        {
+            $splitReq = preg_replace('/^(\w{2})/', '$1 ', $splitReq);
+        }
+
+        $splitReq = preg_replace( '/\s+/', ' ', $splitReq);
+
+        $splitReq = explode(' ', $splitReq);
+
         $aircompany = AirLine::where('airline_iata', $splitReq[0])->first();
         $flight_number = $splitReq[1];
         $booking_class = $splitReq[2];
@@ -20,12 +36,34 @@ class AirPortController extends Controller
         $day_of_the_week = [4];
         $airport_out = AirPort::where('airport_iata_code', substr($splitReq[5],0,3))->first();
         $airport_in = AirPort::where('airport_iata_code', substr($splitReq[5],3,3))->first();
+        $plane = plane::where('code_iata', $splitReq[9])->first();
+        if (!isset($airport_out))
+        {
+            return response()->json(['message'=>"not found airport_out"],404);
+        } else if(!isset($airport_in)) {
+            return response()->json(['message'=>"not found airport_in"],404);
+        }else if(!isset($plane)) {
+            return response()->json(['message' => "not found plane"], 404);
+        }else if(!isset($aircompany)) {
+            return response()->json(['message' => "not found aircompany"], 404);
+        }
         $booking_status = $splitReq[6];
         $splited_Time_departure = explode('+', $splitReq[7]);
         $splited_Time_arrival = explode('+', $splitReq[8]);
+
+        $arrivalDate = '';
+        if (count($splited_Time_arrival) > 1) {
+            $arrivalDate = substr($data, -5, 2);
+            $arrivalMonth = substr($data, -3, 3);
+            $arrivalDate = (int)$arrivalDate + 1 . $arrivalMonth;
+        } else {
+            $arrivalDate = $data;
+        }
+
         $time_departure = substr_replace($splited_Time_departure[0],':',2,0);
         $time_arrival = substr_replace($splited_Time_arrival[0], ':', 2, 0);
-        $plane = plane::where('code_iata', $splitReq[9])->first();
+
+
         $food = $splitReq[10];
         $free_places = $splitReq[11];
         $response = [
@@ -39,15 +77,20 @@ class AirPortController extends Controller
             'booking_status'=>$booking_status,
             'time_departure'=>$time_departure,
             'time_arrival' => $time_arrival,
-            'day_arrival' => $splited_Time_arrival[1],
+            'day_arrival' => $arrivalDate,
             'plane'=>$plane->name,
             'food'=>$food,
             'free_places'=>$free_places,
 
         ];
-            return response()->json($response);
-
+        return response()->json(['data' => $response]);
 
     }
 
+    public function converter(Request $request)
+    {
+//        return response()->json([ => $request->all()]);
+    }
+
 }
+
